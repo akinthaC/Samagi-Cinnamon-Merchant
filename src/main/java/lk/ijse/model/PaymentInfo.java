8ecb46777ab3fbeb5a2ca3322c322a3ce20ef5fb
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-09-23 23:47:12 +0530

    payment info updated

--- src/main/java/lk/ijse/model/PaymentInfo.java
+++ src/main/java/lk/ijse/model/PaymentInfo.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 @Data
 
 public class PaymentInfo {
+    private String supID;
     private String orderNo;
     private String paymentNo;
     private String date;
-    private String totalAmount;
+    private double totalAmount;
     private double payAmount;
     private String description;
     /*private String status;*/
